e4d1f9182241b48824a76b4cb1c01e2deb5b8885
Joachim Jablon <ewjoachim@gmail.com>
2023-01-25 01:22:23 +0100

    Refactor templates & communication: use jinja2 everywhere !

--- tests/unit/test_communication.py
+++ tests/unit/test_communication.py
@@ -1,36 +1,69 @@
-import pytest
-
 from coverage_comment import communication
 
 
-@pytest.mark.parametrize("is_public", [True, False])
-def test_get_readme_and_log(is_public):
+def test_get_readme_and_log__public():
     readme_file, log = communication.get_readme_and_log(
+        is_public=True,
         image_urls={
             "direct": "https://a",
             "endpoint": "https://b",
             "dynamic": "https://c",
         },
         readme_url="https://readme",
-        is_public=is_public,
+        html_report_url="https://html_report",
+        markdown_report="**Hello report!**",
     )
 
     readme = readme_file.contents
 
     assert str(readme_file.path) == "README.md"
 
-    assert "# Coverage data" in readme
+    assert "# Repository Coverage" in readme
 
-    assert "[![Coverage badge](https://a)](https://readme)" in readme
+    assert "[![Coverage badge](https://a)](https://html_report)" in readme
 
-    assert ("[![Coverage badge](https://b)](https://readme)" in readme) is (is_public)
+    assert "[![Coverage badge](https://b)](https://html_report)" in readme
 
-    assert ("[![Coverage badge](https://c)](https://readme)" in readme) is (is_public)
+    assert "[![Coverage badge](https://c)](https://html_report)" in readme
 
     assert "https://a" in log
 
     assert "https://readme" in log
 
-    assert ("https://b" in log) is (is_public)
+    assert "https://b" in log
 
-    assert ("https://c" in log) is (is_public)
+    assert "https://c" in log
+
+
+def test_get_readme_and_log__private():
+    readme_file, log = communication.get_readme_and_log(
+        is_public=False,
+        image_urls={
+            "direct": "https://a",
+            "endpoint": "https://b",
+            "dynamic": "https://c",
+        },
+        readme_url="https://readme",
+        html_report_url="https://html_report",
+        markdown_report="**Hello report!**",
+    )
+
+    readme = readme_file.contents
+
+    assert str(readme_file.path) == "README.md"
+
+    assert "# Repository Coverage" in readme
+
+    assert "[![Coverage badge](https://a)](https://readme)" in readme
+
+    assert "https://b" not in readme
+
+    assert "https://c" not in readme
+
+    assert "https://a" in log
+
+    assert "https://readme" in log
+
+    assert "https://b" not in log
+
+    assert "https://c" not in log
