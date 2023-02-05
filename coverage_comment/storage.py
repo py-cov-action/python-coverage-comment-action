5d6628ac06d171886494e1ebd1c9f18a7be027d8
Joachim Jablon <ewjoachim@gmail.com>
2023-02-05 17:58:20 +0100

    Remove storage.fix_ownership_issues, now fixed in Dockerfile

--- coverage_comment/storage.py
+++ coverage_comment/storage.py
@@ -167,22 +167,3 @@ def get_html_report_url(repository: str, branch: str) -> str:
         repository=repository, branch=branch, path="/htmlcov/index.html"
     )
     return f"https://htmlpreview.github.io/?{readme_url}"
-
-
-def fix_ownership_issues(git: subprocess.Git):
-    # As of 2023-01-30, GitHub changed _something_ to the ownership of the git repo
-    # which confuses git like hell:
-    #
-    #     fatal: detected dubious ownership in repository at '/github/workspace'
-    #     To add an exception for this directory, call:
-    #         git config --global --add safe.directory /github/workspace
-    #
-    # Of course, this makes sense only when the action runs in GHA, but it's harmless
-    # when testing the action locally, except that it will add random trash to the
-    # user's git config.
-    #
-    # From git's doc:
-    # > This config setting is only respected when specified in a system or global
-    # > config, not when it is specified in a repository config or via the command line
-    # > option
-    git.config("--global", "--add", "safe.directory", "/github/workspace")
