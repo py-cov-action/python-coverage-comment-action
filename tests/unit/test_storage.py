8d9abbd61997d9eb24b5f820bdb0da13070b8be4
Joachim Jablon <ewjoachim@gmail.com>
2023-02-08 00:14:37 +0100

    Refactor storage.py
    - "upload_files" is now "commit_operations"
    - it now takes operations instead of files
    - it doesn't deal with branch initialization anymore
    - initialize_branch is gone, moved to on_coverage_branch
    - on_coverage_branch always does git.reset("--hard") before switching
    - on_coverage_branch checks that branch exists before switching
    - on_coverage_branch uses switch instead of checkout
    - on_coverage_branch doesn't yield the branch status anymore

--- tests/unit/test_storage.py
+++ tests/unit/test_storage.py
@@ -6,13 +6,95 @@ import pytest
 from coverage_comment import files, storage
 
 
-def test_initialize_branch(git, tmp_path):
-    readme_path = tmp_path / "readme.txt"
-    git.register("git checkout --orphan foo")()
+def test_on_coverage_branch(git):
+    git.register("git branch --show-current")(stdout="bar")
+    git.register("git fetch")()
     git.register("git reset --hard")()
-    git.register(f"git add {readme_path}")()
+    git.register("git rev-parse --verify foo")()
+    git.register("git switch foo")()
+
+    with storage.on_coverage_branch(git=git, branch="foo"):
+        git.register("git switch bar")()
+
+
+def test_on_coverage_branch__detached_head(git):
+    git.register("git branch --show-current")(exit_code=1)
+    git.register("git rev-parse --short HEAD")(stdout="123abc")
+    git.register("git fetch")()
+    git.register("git reset --hard")()
+    git.register("git rev-parse --verify foo")()
+    git.register("git switch foo")()
+
+    with storage.on_coverage_branch(git=git, branch="foo"):
+        git.register("git switch 123abc")()
+
+
+def test_on_coverage_branch__branch_doesnt_exist(git):
+    git.register("git branch --show-current")(stdout="bar")
+    git.register("git fetch")()
+    git.register("git reset --hard")()
+    git.register("git rev-parse --verify foo")(exit_code=1)
+    git.register("git switch --orphan foo")()
+
+    with storage.on_coverage_branch(git=git, branch="foo"):
+        git.register("git switch bar")()
+
+
+def test_commit_operations__no_diff(git, in_tmp_path):
+    operations = [
+        files.WriteFile(path=pathlib.Path("a.txt"), contents="a"),
+        files.WriteFile(path=pathlib.Path("b.txt"), contents="b"),
+    ]
+
+    # on_coverage_branch
+    git.register("git branch --show-current")(stdout="bar")
+    git.register("git fetch")()
+    git.register("git reset --hard")()
+    git.register("git rev-parse --verify foo")()
+    git.register("git switch foo")()
+
+    # upload_files
+    git.register(f"git add {operations[0].path}")()
+    git.register(f"git add {operations[1].path}")()
+    git.register("git diff --staged --exit-code")()  # no diff
+
+    # __exit__ of on_coverage_branch
+    git.register("git switch bar")()
+
+    storage.commit_operations(
+        operations=operations,
+        git=git,
+        branch="foo",
+    )
+
+    # But content has been written to disk
+    assert operations[0].path.read_text() == operations[0].contents
+    assert operations[1].path.read_text() == operations[1].contents
+
+
+def test_commit_operations(git, in_tmp_path):
+    operations = [
+        files.WriteFile(path=pathlib.Path("a.txt"), contents="a"),
+        files.WriteFile(path=pathlib.Path("b.txt"), contents="b"),
+    ]
+
+    # on_coverage_branch
+    git.register("git branch --show-current")(stdout="bar")
+    git.register("git fetch")()
+    git.register("git reset --hard")()
+    git.register("git rev-parse --verify foo")()
+    git.register("git switch foo")()
+
+    # upload_files
+    git.register(f"git add {operations[0].path}")()
+    git.register(f"git add {operations[1].path}")()
+
+    git.register("git diff --staged --exit-code")(exit_code=1)  # diff!
+
+    # (yes, it's missing the quotes, but this is just an artifact from our test
+    # double)
     git.register(
-        "git commit --message Initialize python-coverage-comment-action special branch",
+        "git commit --message Update badge",
         env={
             "GIT_AUTHOR_NAME": "github-actions",
             "GIT_AUTHOR_EMAIL": "41898282+github-actions[bot]@users.noreply.github.com",
@@ -20,121 +102,19 @@ def test_initialize_branch(git, tmp_path):
             "GIT_COMMITTER_EMAIL": "41898282+github-actions[bot]@users.noreply.github.com",
         },
     )()
-
-    storage.initialize_branch(
-        git=git,
-        branch="foo",
-        initial_file=files.FileWithPath(path=readme_path, contents="bar"),
-    )
-
-    assert readme_path.read_text() == "bar"
-
-
-def test_on_coverage_branch(git):
-    git.register("git branch --show-current")(stdout="bar")
-    git.register("git fetch")()
-    git.register("git checkout foo")()
-
-    with storage.on_coverage_branch(git=git, branch="foo") as exists:
-        assert exists is True
-        git.register("git checkout bar")()
-
-
-def test_on_coverage_branch__detached_head(git):
-    git.register("git branch --show-current")(exit_code=1)
-    git.register("git rev-parse --short HEAD")(stdout="123abc")
-    git.register("git fetch")()
-    git.register("git checkout foo")()
-
-    with storage.on_coverage_branch(git=git, branch="foo") as exists:
-        assert exists is True
-        git.register("git checkout 123abc")()
-
-
-def test_on_coverage_branch__branch_doesnt_exist(git):
-    git.register("git branch --show-current")(stdout="bar")
-    git.register("git fetch")()
-    git.register("git checkout foo")(exit_code=1)
-
-    with storage.on_coverage_branch(git=git, branch="foo") as exists:
-        assert exists is False
-        git.register("git checkout bar")()
-
-
-@pytest.mark.parametrize(
-    "branch_exists, has_diff",
-    [
-        (False, True),
-        (True, False),
-    ],
-)
-def test_upload_files(git, in_tmp_path, branch_exists, has_diff):
-    readme_path = pathlib.Path("readme.txt")
-    initial_file = files.FileWithPath(path=readme_path, contents="foo")
-    files_to_save = [
-        files.FileWithPath(path=pathlib.Path("a.txt"), contents="a"),
-        files.FileWithPath(path=pathlib.Path("b.txt"), contents="b"),
-        files.FileWithPath(path=pathlib.Path("c.txt"), contents=None),
-    ]
-
-    # on_coverage_branch
-    git.register("git branch --show-current")(stdout="bar")
-    git.register("git fetch")()
-    git.register("git checkout foo")(exit_code=0 if branch_exists else 1)
-
-    # I usually hate `if` statements in tests, but we're doing
-    # 2 different very repetitive (almost identical) tests here,
-    # so it really just is a way to make it easier to maintain.
-    # Let's not abuse it.
-    if not branch_exists:
-        # initialize_branch
-        git.register("git checkout --orphan foo")()
-        git.register("git reset --hard")()
-        git.register(f"git add {readme_path}")()
-        git.register(
-            "git commit --message Initialize python-coverage-comment-action special branch",
-            env={
-                "GIT_AUTHOR_NAME": "github-actions",
-                "GIT_AUTHOR_EMAIL": "41898282+github-actions[bot]@users.noreply.github.com",
-                "GIT_COMMITTER_NAME": "github-actions",
-                "GIT_COMMITTER_EMAIL": "41898282+github-actions[bot]@users.noreply.github.com",
-            },
-        )()
-
-    # upload_files
-    git.register(f"git add {files_to_save[0].path}")()
-    git.register(f"git add {files_to_save[1].path}")()
-    git.register(f"git add {files_to_save[2].path}")()
-    git.register("git add readme.txt")()
-    git.register("git diff --staged --exit-code")(exit_code=1 if has_diff else 0)
-
-    if has_diff:
-        # (yes, it's missing the quotes, but this is just an artifact from our test
-        # double)
-        git.register(
-            "git commit --message Update badge",
-            env={
-                "GIT_AUTHOR_NAME": "github-actions",
-                "GIT_AUTHOR_EMAIL": "41898282+github-actions[bot]@users.noreply.github.com",
-                "GIT_COMMITTER_NAME": "github-actions",
-                "GIT_COMMITTER_EMAIL": "41898282+github-actions[bot]@users.noreply.github.com",
-            },
-        )()
-        git.register("git push origin foo")()
+    git.register("git push origin foo")()
 
     # __exit__ of on_coverage_branch
-    git.register("git checkout bar")()
+    git.register("git switch bar")()
 
-    storage.upload_files(
-        files=files_to_save,
+    storage.commit_operations(
+        operations=operations,
         git=git,
         branch="foo",
-        initial_file=initial_file,
     )
 
-    assert files_to_save[0].path.read_text() == files_to_save[0].contents
-    assert files_to_save[1].path.read_text() == files_to_save[1].contents
-    assert not files_to_save[2].path.exists()
+    assert operations[0].path.read_text() == operations[0].contents
+    assert operations[1].path.read_text() == operations[1].contents
 
 
 def test_get_datafile_contents__not_found(gh, session):
