8b6a8f4aec32cc6d1b824eea6f22e2545f17ea41
Martin Bernstorff <martinbernstorff@gmail.com>
2023-06-13 09:49:23 +0200

    tests: remove multi-line test for diff_coverage
    Identical to prior tests

--- tests/unit/test_annotations.py
+++ tests/unit/test_annotations.py
@@ -12,16 +12,3 @@ def test_annotations(diff_coverage_obj, capsys):
 ::endgroup::"""
     output = capsys.readouterr()
     assert output.out.strip() == expected
-
-
-def test_annotations_several_files(diff_coverage_obj, capsys):
-    annotations.create_pr_annotations(
-        annotation_type="notice", coverage=diff_coverage_obj
-    )
-
-    expected = """::group::Annotations of lines with missing coverage
-::notice file=codebase/code.py,line=7::This line has no coverage
-::notice file=codebase/code.py,line=9::This line has no coverage
-::endgroup::"""
-    output = capsys.readouterr()
-    assert output.out.strip() == expected
