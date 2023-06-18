6a904d1768dce54fec58f6d44f9a2a5e9ce77afe
kieferro <81773954+kieferro@users.noreply.github.com>
2023-06-18 15:00:37 +0000

    Revert "feat: create annotate only from diff coverage" (#193)

--- coverage_comment/annotations.py
+++ coverage_comment/annotations.py
@@ -4,17 +4,15 @@ from coverage_comment import github
 MISSING_LINES_GROUP_TITLE = "Annotations of lines with missing coverage"
 
 
-def create_pr_annotations(annotation_type: str, coverage: coverage_module.DiffCoverage):
+def create_pr_annotations(annotation_type: str, coverage: coverage_module.Coverage):
     github.send_workflow_command(
         command="group", command_value=MISSING_LINES_GROUP_TITLE
     )
 
-    for filepath, file_coverage in coverage.files.items():
-        for missing_line in file_coverage.violation_lines:
+    for filename, file_coverage in coverage.files.items():
+        for missing_line in file_coverage.missing_lines:
             github.create_missing_coverage_annotation(
-                annotation_type=annotation_type,
-                file=str(filepath),
-                line=missing_line,
+                annotation_type=annotation_type, file=filename, line=missing_line
             )
 
     github.send_workflow_command(command="endgroup", command_value="")
--- tests/unit/test_annotations.py
+++ tests/unit/test_annotations.py
@@ -1,10 +1,8 @@
 from coverage_comment import annotations
 
 
-def test_annotations(diff_coverage_obj, capsys):
-    annotations.create_pr_annotations(
-        annotation_type="warning", coverage=diff_coverage_obj
-    )
+def test_annotations(coverage_obj, capsys):
+    annotations.create_pr_annotations(annotation_type="warning", coverage=coverage_obj)
 
     expected = """::group::Annotations of lines with missing coverage
 ::warning file=codebase/code.py,line=7::This line has no coverage
@@ -14,21 +12,20 @@ def test_annotations(diff_coverage_obj, capsys):
     assert output.out.strip() == expected
 
 
-def test_annotations_several_files(diff_coverage_obj_many_missing_lines, capsys):
+def test_annotations_several_files(coverage_obj_many_missing_lines, capsys):
     annotations.create_pr_annotations(
-        annotation_type="notice", coverage=diff_coverage_obj_many_missing_lines
+        annotation_type="notice", coverage=coverage_obj_many_missing_lines
     )
 
     expected = """::group::Annotations of lines with missing coverage
-::notice file=codebase/code.py,line=3::This line has no coverage
-::notice file=codebase/code.py,line=5::This line has no coverage
-::notice file=codebase/code.py,line=21::This line has no coverage
-::notice file=codebase/code.py,line=111::This line has no coverage
-::notice file=codebase/helper.py,line=19::This line has no coverage
-::notice file=codebase/helper.py,line=22::This line has no coverage
-::notice file=codebase/files.py,line=120::This line has no coverage
-::notice file=codebase/files.py,line=121::This line has no coverage
-::notice file=codebase/files.py,line=122::This line has no coverage
+::notice file=codebase/main.py,line=3::This line has no coverage
+::notice file=codebase/main.py,line=7::This line has no coverage
+::notice file=codebase/main.py,line=13::This line has no coverage
+::notice file=codebase/main.py,line=21::This line has no coverage
+::notice file=codebase/main.py,line=123::This line has no coverage
+::notice file=codebase/caller.py,line=13::This line has no coverage
+::notice file=codebase/caller.py,line=21::This line has no coverage
+::notice file=codebase/caller.py,line=212::This line has no coverage
 ::endgroup::"""
     output = capsys.readouterr()
     assert output.out.strip() == expected
