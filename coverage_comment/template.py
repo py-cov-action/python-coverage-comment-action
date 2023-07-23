4010b5f4d04a82475422ed3f5003b58bcc7392ef
Joachim Jablon <ewjoachim@gmail.com>
2023-07-23 18:12:42 +0200

    Use SUBPROJECT_ID

--- coverage_comment/template.py
+++ coverage_comment/template.py
@@ -7,7 +7,9 @@ from jinja2.sandbox import SandboxedEnvironment
 
 from coverage_comment import coverage as coverage_module
 
-MARKER = """<!-- This comment was produced by python-coverage-comment-action -->"""
+MARKER = (
+    """<!-- This comment was produced by python-coverage-comment-action{id_part} -->"""
+)
 
 
 def uptodate():
@@ -39,11 +41,16 @@ class TemplateError(Exception):
     pass
 
 
+def get_marker(marker_id: str | None):
+    return MARKER.format(id_part=f" (id: {marker_id})" if marker_id else "")
+
+
 def get_comment_markdown(
     coverage: coverage_module.Coverage,
     diff_coverage: coverage_module.DiffCoverage,
     previous_coverage_rate: decimal.Decimal | None,
     base_template: str,
+    marker: str,
     custom_template: str | None = None,
 ):
     loader = CommentLoader(base_template=base_template, custom_template=custom_template)
@@ -55,12 +62,12 @@ def get_comment_markdown(
             previous_coverage_rate=previous_coverage_rate,
             coverage=coverage,
             diff_coverage=diff_coverage,
-            marker=MARKER,
+            marker=marker,
         )
     except jinja2.exceptions.TemplateError as exc:
         raise TemplateError from exc
 
-    if MARKER not in comment:
+    if marker not in comment:
         raise MissingMarker()
 
     return comment
