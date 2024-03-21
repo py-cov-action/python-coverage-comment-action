32be517f35554c27dece26f6aa44a738a7cfc0c1
Keunhyung Chung <axdwa777@gmail.com>
2024-03-21 19:29:49 +0900

    Use f string

--- coverage_comment/coverage.py
+++ coverage_comment/coverage.py
@@ -194,12 +194,7 @@ def extract_info(data: dict, coverage_path: pathlib.Path) -> Coverage:
                 info=CoverageInfo(
                     covered_lines=file_data["summary"]["covered_lines"],
                     num_statements=file_data["summary"]["num_statements"],
-                    percent_covered=decimal.Decimal(
-                        str(
-                            round(file_data["summary"]["percent_covered"], ndigits=2)
-                            / 100
-                        )
-                    ),
+                    percent_covered=decimal.Decimal(f"{file_data["summary"]["percent_covered"]}") / 100,
                     missing_lines=file_data["summary"]["missing_lines"],
                     excluded_lines=file_data["summary"]["excluded_lines"],
                     num_branches=file_data["summary"].get("num_branches"),
@@ -215,9 +210,7 @@ def extract_info(data: dict, coverage_path: pathlib.Path) -> Coverage:
         info=CoverageInfo(
             covered_lines=data["totals"]["covered_lines"],
             num_statements=data["totals"]["num_statements"],
-            percent_covered=decimal.Decimal(
-                str(round(data["totals"]["percent_covered"], ndigits=2) / 100)
-            ),
+            percent_covered=decimal.Decimal(f"{data["totals"]["percent_covered"]}") / 100,
             missing_lines=data["totals"]["missing_lines"],
             excluded_lines=data["totals"]["excluded_lines"],
             num_branches=data["totals"].get("num_branches"),
